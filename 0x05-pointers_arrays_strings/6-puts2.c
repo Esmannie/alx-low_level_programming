d0c3919c141b862cab6e48b13e9d17a039a9d795
Esmannie <issyemerald@gmail.com>
2022-12-19 12:45:41 -0800

    pointer

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
-/**
- *puts2 - prints every other character in a string beginning with the first one
- *
+                                                                                        /**
+ *puts2 - prints every other character in a string beginning with the first one          *
  *@str: pointer that will store the string's location
  *
  *Return: nothing
  */
 void puts2(char *str)
 {
-	int cycle;
+        int cycle;
 
-	while (*str != 0)
-	{
-		if (cycle % 2 == 0)
-		{
-			_putchar(*str);
-		}
-		cycle++;
-		str++;
-	}
-	_putchar(10);
-}
+        while (*str != 0)
+        {
+                if (cycle % 2 == 0)
+                {                                                                                               _putchar(*str);
+                }
+                cycle++;
+                str++;
+        }
+        _putchar(10);
